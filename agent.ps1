035a6dcafd7c256c814d0c1450db5fba287ca651
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-03-10 18:09:04 -0500

    Try another fix

--- agent.ps1
+++ agent.ps1
@@ -1,2 +1,4 @@
-Register-ScheduledTask -TaskName 'BreakGA' -Trigger (New-ScheduledTaskTrigger -AtStartup) -User "brokenGA\AzureAdmin" -Action (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& { cmd /c "sc config rdagent start= disabled"; cmd /c "sc stop rdagent"; cmd /c "sc config WindowsAzureGuestAgent start= disabled"; cmd /c "sc stop WindowsAzureGuestAgent" }"') -RunLevel Highest –Force; 
+$trigger = New-ScheduledTaskTrigger -AtStartup;
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "& {cmd /c \"sc config rdagent start= disabled\"; cmd /c \"sc stop rdagent\"; cmd /c \"sc config WindowsAzureGuestAgent start= disabled\"; cmd /c \"sc stop WindowsAzureGuestAgent\"}"';
+Register-ScheduledTask -TaskName 'BreakGA' -Trigger $trigger -Action $action -User "brokenGA\AzureAdmin" -RunLevel Highest -Force;
 Start-ScheduledTask 'BreakGA';
