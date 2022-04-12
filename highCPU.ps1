fc9a4fae3f5fd62014fd5c85f199cac99f90f46e
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-04-12 14:24:56 -0400

    Update highCPU.ps1

--- highCPU.ps1
+++ highCPU.ps1
@@ -1,1 +1,1 @@
-$Script:StressJob = 1..$env:NUMBER_OF_PROCESSORS | foreach {Start-Job {Start-Sleep -Seconds 120;[System.Threading.Thread]::CurrentThread.Priority = 'Highest';while ($true) {$i++};}}
+$Script:StressJob = 1..$env:NUMBER_OF_PROCESSORS | foreach {Start-Job {[System.Threading.Thread]::CurrentThread.Priority = 'Highest';while ($true) {$i++};}}
