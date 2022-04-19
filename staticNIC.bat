eef62f794d5d3d86a8babcd52ffdca99847059bd
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-04-19 17:38:35 -0400

    Update to run as job (deployment was failing before)

--- staticNIC.bat
+++ staticNIC.bat
@@ -1,1 +1,1 @@
-powershell -noexit "New-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name Ethernet*)[0].ifIndex -IPAddress 8.8.8.8"
+powershell -noexit "Start-Job { Start-Sleep -seconds 60; New-NetIPAddress -InterfaceIndex (Get-NetAdapter -Name Ethernet*)[0].ifIndex -IPAddress 8.8.8.8; }"
