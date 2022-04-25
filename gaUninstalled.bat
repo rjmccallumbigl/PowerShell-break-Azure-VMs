6c86b0ef956bd72753a466b2aa93a67a0329fb11
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-04-25 17:24:10 -0400

    Update gaUninstalled.bat

--- gaUninstalled.bat
+++ gaUninstalled.bat
@@ -1,1 +1,2 @@
-sc config rdagent start= disabled && sc stop rdagent && sc config WindowsAzureGuestAgent start= disabled && sc stop WindowsAzureGuestAgent && sc delete rdagent && sc delete WindowsAzureGuestAgent && rmdir C:\WindowsAzure /S /Q
+sc config rdagent start= disabled && sc stop rdagent && sc config WindowsAzureGuestAgent start= disabled && sc stop WindowsAzureGuestAgent && sc delete rdagent && sc delete WindowsAzureGuestAgent
+rmdir C:\WindowsAzure /S /Q
