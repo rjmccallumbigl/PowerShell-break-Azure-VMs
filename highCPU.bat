42db4f7147daaf82e496a49b2d657e1a8ad12b8b
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-05-03 16:52:31 -0400

    Update highCPU.bat

--- highCPU.bat
+++ highCPU.bat
@@ -1,1 +1,11 @@
+@REM Download PerfInsights
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://aka.ms/perfinsightslatest', 'C:\Program Files\Common Files\PerfInsights.zip')" 
+
+@REM Expand zip
+powershell "Expand-Archive -LiteralPath 'C:\Program Files\Common Files\PerfInsights.zip' -DestinationPath 'C:\Program Files\Common Files\PerfInsights'"
+
+@REM asynchronously start PerfInsights for 300s
+powershell "Start-Process powershell -Verb runAs -ArgumentList {& 'C:\Program Files\Common Files\PerfInsights\PerfInsights.exe' /run advanced xpns /d 300 /AcceptDisclaimerAndShareDiagnostics /sau /outputfolder 'C:\Program Files\Common Files'}"
+
+@REM Increase CPU utilization
 powershell -noexit "$Script:StressJob = 1..$env:NUMBER_OF_PROCESSORS | foreach {Start-Job {[System.Threading.Thread]::CurrentThread.Priority = 'Highest';while ($true) {$i++};}}"
