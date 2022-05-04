f36ccbad8bf025baca9171f9a4ede68a70f81dc6
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-05-04 15:27:58 -0400

    Trigger crash using NotMyFault

--- crash.bat
+++ crash.bat
@@ -2,17 +2,16 @@
 
 @REM Download files
 powershell "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12" 
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/rjmccallumbigl/PowerShell-break-Azure-VMs/raw/main/Consume.exe', 'C:\Program Files\Common Files\consume.exe')
 powershell "(New-Object System.Net.WebClient).DownloadFile('https://github.com/Azure/repair-script-library/raw/master/src/windows/common/tools/kdbgctrl.exe', 'C:\Program Files\Common Files\kdbgctrl.exe')
+powershell "(New-Object System.Net.WebClient).DownloadFile('https://download.sysinternals.com/files/NotMyFault.zip', 'C:\Program Files\Common Files\NotMyFault.zip')
+
+@REM Expand zips
+powershell "Expand-Archive -LiteralPath 'C:\Program Files\Common Files\NotMyFault.zip' -DestinationPath 'C:\Program Files\Common Files\NotMyFault'"
 
 @REM trigger dump config
 powershell "Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -Name 'DedicatedDumpFile' -Value 'D:\dd.sys'"
 powershell "Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -Name 'DumpFile' -Value '%SystemRoot%\Memory.dmp'"
 powershell "& 'C:\Program Files\Common Files\kdbgctrl.exe' -sd full"
 
-@REM asynchronously start Consume
-powershell "Start-Process powershell -Verb runAs -ArgumentList {& 'C:\Program Files\Common Files\Consume.exe' -kernel-pool }"
-powershell "Start-Process powershell -Verb runAs -ArgumentList {& 'C:\Program Files\Common Files\Consume.exe' -physical-memory }"
-powershell "Start-Process powershell -Verb runAs -ArgumentList {& 'C:\Program Files\Common Files\Consume.exe' -cpu-time }"
-powershell "Start-Process powershell -Verb runAs -ArgumentList {& 'C:\Program Files\Common Files\Consume.exe' -page-file }"
-
+@REM Trigger crash
+powershell "& 'C:\Program Files\Common Files\NotMyFault\NotMyFault.exe' /crash" -accepteula
